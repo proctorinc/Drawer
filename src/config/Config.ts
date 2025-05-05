3e8f6f65a6f7a7cc7e1fd4f5bc83ca496ac85de9
Matt Proctor <proctormatt0@gmail.com>
2025-05-05 13:08:02 -0700

    Changed server URL to environment variable

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -1,5 +1,6 @@
 export const Config = {
-    API_BASE_URL: "http://localhost:8002",
+    // Get from vite environment variable
+    API_BASE_URL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8002",
     CURSOR_SIZE: 32,
     CANVAS_WIDTH: 370,
     CANVAS_HEIGHT: 370,
