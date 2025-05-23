6c2eb7435b15a86eef681687b7e3f41b1c1cdc5a
Matt Proctor <proctormatt0@gmail.com>
2025-05-23 14:48:23 -0700

    Update friends and friends list

--- src/pages/AddFriendPage.tsx
+++ src/pages/AddFriendPage.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { useParams } from '@tanstack/react-router';
+import { useNavigate, useParams } from '@tanstack/react-router';
 import { addFriend } from '@/api/Api';
 
 const AddFriendPage: React.FC = () => {
+    const navigate = useNavigate()
     const { userId } = useParams({ from: "/app/add-friend/$userId"});
     const [error, setError] = useState('');
     const [success, setSuccess] = useState('');
@@ -15,6 +16,7 @@ const AddFriendPage: React.FC = () => {
         try {
             await addFriend(userId);
             setSuccess('Friend added successfully!');
+            navigate({ to: "/app/user-profile" })
         } catch (err) {
             setError('Error adding friend: ' + (err as Error).message);
         }
@@ -22,15 +24,24 @@ const AddFriendPage: React.FC = () => {
 
     return (
         <div className="flex flex-col items-center p-4 gap-4 bg-gray-100 min-h-screen">
-            <h1 className="text-2xl">Add a Friend</h1>
-            <form onSubmit={handleAddFriend} className="flex flex-col gap-4">
-                {error && <p className="text-red-500">{error}</p>}
-                {success && <p className="text-green-500">{success}</p>}
-                <p>Are you sure you want to add user with ID: {userId}?</p>
-                <button type="submit" className="bg-blue-500 cursor-pointer text-white p-2 rounded">
-                    Add Friend
-                </button>
-            </form>
+            <div className="flex flex-col justify-center items-center w-full gap-4 flex-grow">
+                <div className="flex justify-between items-center bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-2xl shadow-md px-6 py-2 w-full max-w-md font-bold">
+                    <div className="flex flex-col">
+                        <h1 className="text-2xl text-gray-900 dark:text-gray-100">Add a friend!</h1>
+                    </div>
+                </div>
+                <form onSubmit={handleAddFriend} className="flex flex-col gap-4 bg-white border border-gray-300 rounded-2xl w-full max-w-md p-4">
+                    {error && <p className="text-red-500">{error}</p>}
+                    {success && <p className="text-green-500">{success}</p>}
+                    <p>Are you sure you want to add user with ID: {userId}?</p>
+                    <button
+                            type="submit"
+                            className="flex justify-center cursor-pointer disabled:cursor-default disabled:scale-100 hover:scale-110 transition-all duration-300 text-lg gap-2 items-center bg-gradient-to-tr from-blue-600 to-purple-600 font-bold px-6 py-3 rounded-2xl shadow-md disabled:opacity-50"
+                        >
+                            Add Friend
+                        </button>
+                </form>
+            </div>
         </div>
     );
 };
--- src/profile/components/FriendList.tsx
+++ src/profile/components/FriendList.tsx
@@ -1,5 +1,7 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useProfile } from "../UserProfileContext";
 import { UserProfileIcon } from "./UserProfileIcon";
+import { faShare } from "@fortawesome/free-solid-svg-icons";
 
 export const FriendList = () => {
     const { userProfile } = useProfile();
@@ -8,11 +10,32 @@ export const FriendList = () => {
         return <></>;
     }
 
+    const handleShare = async () => {
+        if (navigator.share) {
+            try {
+            await navigator.share({
+                text: `Hey, let's draw! ${window.location.origin}/app/add-friend/${userProfile.user.id}`,
+            });
+                console.log('Data was shared successfully');
+            } catch (error) {
+                console.error('Sharing failed:', error);
+            }
+        } else {
+            alert('Web Share API is not supported on this device/browser.');
+            navigator.clipboard.writeText(`Hey, let's draw! ${window.location.origin}/app/add-friend/${userProfile.user.id}`);
+        }
+    };
+
     return (
         <div className="flex flex-col gap-4 bg-white border border-gray-200 rounded-2xl w-full max-w-md p-4">
-            <div className="border-gray-200">
-                <h3 className="text-lg font-bold">Friend list</h3>
-                <p className="text-sm text-gray-500">My friends</p>
+            <div className="flex justify-between items-center">
+                <div className="border-gray-200">
+                    <h3 className="text-lg font-bold">Friend list</h3>
+                    <p className="text-sm text-gray-500">My friends</p>
+                </div>
+                <button className="flex items-center bg-gray-100 text-gray-700 cursor-pointer hover:bg-gray-200 hover:text-gray-900 hover:scale-110 transition-all duration-300 border border-gray-200 justify-center w-10 h-10 rounded-xl border-gray-200" onClick={handleShare}>
+                    <FontAwesomeIcon icon={faShare} />
+                </button>
             </div>
             {userProfile.friends.length === 0 && <div className="border border-gray-200 p-4 bg-gray-100 rounded-2xl">None</div>}
             {userProfile.friends.length > 0 && (
