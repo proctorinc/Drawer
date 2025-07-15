4f8953c7607ce78dc0afc3b8598b77fd431ff68f
Matt Proctor <proctormatt0@gmail.com>
2025-07-15 12:53:52 -0700

    Fix for UI bug

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -12,12 +12,12 @@ const Layout: FC<Props> = ({ header, children }) => {
   const { isAuthenticated } = useAuth();
 
   return (
-    <div className="flex flex-col items-center justify-center">
-      <div className="sticky top-0 flex flex-col max-w-md w-full gap-6 md:p-2">
+    <div className=" flex flex-col items-center justify-center">
+      <div className="-z-10 sticky top-0 flex flex-col max-w-md w-full gap-6 md:p-2">
         <Header />
         {header}
       </div>
-      <div className="relative flex flex-col flex-grow w-full items-center p-6 md:p-2 gap-6 bg-base min-h-screen overflow-y-auto overflow-x-hidden pb-30 max-w-md rounded-t-3xl border-2 border-border">
+      <div className="z-0 relative flex flex-col flex-grow w-full items-center p-6 md:p-2 gap-6 bg-base min-h-screen overflow-y-auto overflow-x-hidden pb-30 max-w-md rounded-t-3xl border-2 border-border">
         {children}
         {isAuthenticated && <Navbar />}
       </div>
