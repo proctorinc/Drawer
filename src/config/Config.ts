624dab7a2487f00d0a196232a190d1f7291abfce
Matt Proctor <proctormatt0@gmail.com>
2025-07-17 21:23:42 -0700

    adding public api key. HARDCODED yikes. I hate envvars

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -1,7 +1,9 @@
 export const Config = {
   ENV: import.meta.env.VITE_ENV || 'production',
   API_BASE_URL: import.meta.env.VITE_API_BASE_URL || '/api/v1',
-  VAPID_PUBLIC_KEY: import.meta.env.VITE_VAPID_PUBLIC_KEY || '',
+  VAPID_PUBLIC_KEY:
+    import.meta.env.VITE_VAPID_PUBLIC_KEY ||
+    'BPA-gbCMFRfVEVwaxNuZQ06iWePK3pgeIXwC2QqlOcCvInoWzH363rt9R9n-tffjgq_vJUSBpa4sl2P2It5EVYY',
   APP_NAME: 'The Daily Doodle',
   CURSOR_SIZE: 20,
   ERASER_COLOR: '#f5f4f0',
